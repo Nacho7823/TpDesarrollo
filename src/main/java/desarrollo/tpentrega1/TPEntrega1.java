f8e293e93dea3e7935e1894638e1d4d5cf3bcc22
N4CH078 <ignacios07082003@gmail.com>
2024-08-23 15:54:22 -0300

    update main

--- src/main/java/desarrollo/tpentrega1/TPEntrega1.java
+++ src/main/java/desarrollo/tpentrega1/TPEntrega1.java
@@ -4,12 +4,40 @@
 
 package desarrollo.tpentrega1;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import desarrollo.tpentrega1.entidades.Coordenada;
+import desarrollo.tpentrega1.entidades.Vendedor;
 
 /**
  *
  * @author florh
  */
 public class TPEntrega1 {
+
+
+    void etapa1() {
+        ArrayList<Vendedor> vendedores = new ArrayList<>();
+        ArrayList<Coordenada> coords;
+
+        coords = new ArrayList<Coordenada>();
+        coords.add(new Coordenada(1.23, 2.34));
+        vendedores.add(new Vendedor("0",  "Matias Sandria",  "San Martin 1263", coords));
+
+
+        coords = new ArrayList<Coordenada>();
+        coords.add(new Coordenada(3.45, 4.56));
+        vendedores.add(new Vendedor("1", "Exequiel sandria", "corrientes 1234", coords));
+
+        coords = new ArrayList<Coordenada>();
+        coords.add(new Coordenada(4.12, 3.41));
+        vendedores.add(new Vendedor("2", "Flor Hiembuchner", "san martin 5412", coords));
+        
+        
+
+    }
+
 
     public static void main(String[] args) {
         
--- src/main/java/desarrollo/tpentrega1/servicios/VendedorServicio.java
+++ src/main/java/desarrollo/tpentrega1/servicios/VendedorServicio.java
@@ -9,5 +9,20 @@ package desarrollo.tpentrega1.servicios;
  * @author florh
  */
 public class VendedorServicio {
-    
+    // creame el servicio de vendedor
+
+    public void crearVendedor() {
+        // crear un vendedor
+    }
+
+    public void modificarVendedor() {
+        // modificar un vendedor
+    }
+
+    public void eliminarVendedor() {
+        // eliminar un vendedor
+    }
+
+
+
 }
