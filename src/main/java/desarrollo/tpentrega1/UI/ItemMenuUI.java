9a0fb890964c0a85cc7857547a08f2cecca0d14d
mattcarza <mattcarza2002@gmail.com>
2024-11-02 12:55:06 -0300

    color itemmenu

--- src/main/java/desarrollo/tpentrega1/UI/ItemMenuUI.java
+++ src/main/java/desarrollo/tpentrega1/UI/ItemMenuUI.java
@@ -43,7 +43,7 @@ public class ItemMenuUI extends JPanel{
       JScrollPane scrollPaneBebida = new JScrollPane(tableBebida);
       JScrollPane scrollPanePlato = new JScrollPane(tablePlato);
       
-      this.setBackground(Color.cyan.darker());
+      this.setBackground(new Color(130, 217, 217));
      
       GroupLayout layout= new GroupLayout(this);
       this.setLayout(layout);
