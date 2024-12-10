ed8c8a4d305be820da7957b991ecff0c4b61d27d
Ignacio Sacco <ignacios07082003@gmail.com>
2024-12-10 09:46:44 -0300

    creating interfaces

---
+++ src/main/resources/templates/index.js
@@ -0,0 +1,7 @@
+btnVendedor = document.getElementById("btn-vendedor")
+
+btnVendedor.addEventListener("click", () => {
+    window.location.href = "./vendedor/vendedor.html"
+    // window.location.href = "https://localhost/vendedor"
+})
+
---
+++ src/main/resources/templates/vendedor/vendedor.js
@@ -0,0 +1,8 @@
+
+
+btnVolver = document.getElementById("btn-volver")
+
+
+btnVolver.addEventListener("click", () => {
+    window.location.href="../index.html"
+})
