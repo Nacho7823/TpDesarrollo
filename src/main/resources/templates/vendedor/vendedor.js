38af30c8ef7c21aa6f6d02245363a17de1fd107c
Ignacio Sacco <ignacios07082003@gmail.com>
2024-12-11 15:32:51 -0300

    complete vendedor ui

--- src/main/resources/templates/vendedor/vendedor.js
+++ src/main/resources/templates/vendedor/vendedor.js
@@ -1,19 +1,13 @@
-btnVolver = document.getElementById("btn-volver")
-
-btnVolver.addEventListener("click", () => {
-    window.location.href="../index.html"
-})
-
 server = window.location.origin;
 
 btnBuscar = document.getElementById("btn-buscar")
-btnEditar = document.getElementById("btn-editar")
-btnEliminar = document.getElementById("btn-eliminar")
+// btnEditar = document.getElementById("btn-editar")
+// btnEliminar = document.getElementById("btn-eliminar")
 btnCrear = document.getElementById("btn-crear")
 
 
 btnBuscar.addEventListener("click", async () => {
-    
+
     try {
         const response = await fetch(server + "/vendedores");
         if (!response.ok) {
@@ -23,7 +17,184 @@ btnBuscar.addEventListener("click", async () => {
         const data = await response.json();
         alert(data);
 
-    } catch(error) {
+    } catch (error) {
         alert(error);
     }
 });
+
+
+def = [{
+        "id_cliente": 1,
+        "nombre": "Luis Perez",
+        "direccion": "Dorrego 123",
+        "longitud": 0.4,
+        "latitud": 0.1
+    }, 
+    {
+        "id_cliente": 1,
+        "nombre": "Luis Perez",
+        "direccion": "Dorrego 123",
+        "longitud": 0.4,
+        "latitud": 0.1
+    },
+    {
+        "id_cliente": 1,
+        "nombre": "Luis Perez",
+        "direccion": "Dorrego 123",
+        "longitud": 0.4,
+        "latitud": 0.1
+    },
+    {
+        "id_cliente": 1,
+        "nombre": "Luis Perez",
+        "direccion": "Dorrego 123",
+        "longitud": 0.4,
+        "latitud": 0.1
+    },
+    {
+        "id_cliente": 1,
+        "nombre": "Luis Perez",
+        "direccion": "Dorrego 123",
+        "longitud": 0.4,
+        "latitud": 0.1
+    },
+    {
+        "id_cliente": 1,
+        "nombre": "Luis Perez",
+        "direccion": "Dorrego 123",
+        "longitud": 0.4,
+        "latitud": 0.1
+    },
+    {
+        "id_cliente": 1,
+        "nombre": "Luis Perez",
+        "direccion": "Dorrego 123",
+        "longitud": 0.4,
+        "latitud": 0.1
+    },
+    {
+        "id_cliente": 1,
+        "nombre": "Luis Perez",
+        "direccion": "Dorrego 123",
+        "longitud": 0.4,
+        "latitud": 0.1
+    },
+    {
+        "id_cliente": 1,
+        "nombre": "Luis Perez",
+        "direccion": "Dorrego 123",
+        "longitud": 0.4,
+        "latitud": 0.1
+    },
+    {
+        "id_cliente": 1,
+        "nombre": "Luis Perez",
+        "direccion": "Dorrego 123",
+        "longitud": 0.4,
+        "latitud": 0.1
+    },
+    {
+        "id_cliente": 1,
+        "nombre": "Luis Perez",
+        "direccion": "Dorrego 123",
+        "longitud": 0.4,
+        "latitud": 0.1
+    },
+    {
+        "id_cliente": 1,
+        "nombre": "Luis Perez",
+        "direccion": "Dorrego 123",
+        "longitud": 0.4,
+        "latitud": 0.1
+    },
+    {
+        "id_cliente": 1,
+        "nombre": "Luis Perez",
+        "direccion": "Dorrego 123",
+        "longitud": 0.4,
+        "latitud": 0.1
+    },
+    {
+        "id_cliente": 1,
+        "nombre": "Luis Perez",
+        "direccion": "Dorrego 123",
+        "longitud": 0.4,
+        "latitud": 0.1
+    },
+    {
+        "id_cliente": 1,
+        "nombre": "Luis Perez",
+        "direccion": "Dorrego 123",
+        "longitud": 0.4,
+        "latitud": 0.1
+    },
+    {
+        "id_cliente": 1,
+        "nombre": "Luis Perez",
+        "direccion": "Dorrego 123",
+        "longitud": 0.4,
+        "latitud": 0.1
+    },
+    {
+        "id_cliente": 1,
+        "nombre": "Luis Perez",
+        "direccion": "Dorrego 123",
+        "longitud": 0.4,
+        "latitud": 0.1
+    },
+    {
+        "id_cliente": 1,
+        "nombre": "Luis Perez",
+        "direccion": "Dorrego 123",
+        "longitud": 0.4,
+        "latitud": 0.1
+    },
+    {
+        "id_cliente": 1,
+        "nombre": "Luis Perez",
+        "direccion": "Dorrego 123",
+        "longitud": 0.4,
+        "latitud": 0.1
+    },
+    {
+        "id_cliente": 1,
+        "nombre": "Luis Perez",
+        "direccion": "Dorrego 123",
+        "longitud": 0.4,
+        "latitud": 0.1
+    },
+    {
+        "id_cliente": 1,
+        "nombre": "Luis Perez",
+        "direccion": "Dorrego 123",
+        "longitud": 0.4,
+        "latitud": 0.1
+    },
+    {
+        "id_cliente": 1,
+        "nombre": "Luis Perez",
+        "direccion": "Dorrego 123",
+        "longitud": 0.4,
+        "latitud": 0.1
+    }
+];
+
+def.forEach(e => {
+    addRow(e)
+})
+
+function addRow(vendedor) {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+                <th>${vendedor.id_cliente}</th>
+                <th>${vendedor.nombre}</th>
+                <th>${vendedor.direccion}</th>
+                <th>${vendedor.longitud}</th>
+                <th>${vendedor.latitud}</th>
+                <th>editar</th>
+                <th>eliminar</th>
+            `;
+
+    const tbod = document.getElementById("tablebody");
+    tbod.appendChild(row)
+}
