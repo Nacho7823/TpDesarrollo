69da77df706e1cd364f135e185813f796d72faea
Ignacio Sacco <ignacios07082003@gmail.com>
2024-12-11 14:52:30 -0300

    update ui

--- target/classes/templates/index.js
+++ target/classes/templates/index.js
@@ -1,7 +1,15 @@
-btnVendedor = document.getElementById("btn-vendedor")
+btnVendedor = document.getElementById("btn-vendedor");
+btnCliente = document.getElementById("btn-cliente");
+btnPedidos = document.getElementById("btn-pedidos");
+btnItems = document.getElementById("btn-items");
 
 btnVendedor.addEventListener("click", () => {
     window.location.href = "./vendedor/vendedor.html"
+    // window.location.href = "https://localhost/vendedor"
+})
+
+btnCliente.addEventListener("click", () => {
+    window.location.href = "./cliente/cliente.html"
     // window.location.href = "https://localhost/vendedor"
 })
 
