614ca557619210272b62e263be3789c89136bb5b
Florencia Hiembuchner <florhiembuchner@gmail.com>
2024-12-13 19:28:03 -0300

    cambios en entidades y services

---
+++ src/main/java/com/desarrollo/tpSpring/services/PedidoService.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.desarrollo.tpSpring.services;
+
+/**
+ *
+ * @author florh
+ */
+public class PedidoService {
+    
+}
