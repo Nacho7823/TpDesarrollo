199bbf1b050ddc44af7b924496d34a0c9854337e
Florencia Hiembuchner <florhiembuchner@gmail.com>
2024-12-14 11:17:42 -0300

    crud de Pedido

--- src/main/java/com/desarrollo/tpSpring/services/PedidoService.java
+++ src/main/java/com/desarrollo/tpSpring/services/PedidoService.java
@@ -4,7 +4,12 @@
  */
 package com.desarrollo.tpSpring.services;
 
+import com.desarrollo.tpSpring.DAOs.PedidoRepository;
+import com.desarrollo.tpSpring.entities.Pedido;
+import jakarta.transaction.Transactional;
+import java.util.List;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
 /**
  *
@@ -12,5 +17,39 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class PedidoService {
+    private final PedidoRepository pedidoRepository;
+
+    public PedidoService(PedidoRepository pedidoRepository) {
+        this.pedidoRepository = pedidoRepository;
+    }
+    
+    @Transactional
+    public void crearPedido(@Validated Pedido pedido){
+        if(pedido.getPago() != null){
+            pedidoRepository.save(pedido);
+           
+        }
+    }
+    
+    public List<Pedido> obtenerPedidos(){
+        return pedidoRepository.findAll();
+    }
+    
+    public Pedido buscarPedidoPorId(int id){
+        return pedidoRepository.findById(id).get();
+    }
+    
+    @Transactional
+    public void actualizarPedido(@Validated Pedido pedido){
+        if(pedido.getPago() != null){
+            pedidoRepository.save(pedido);
+           
+        }
+    }
+    
+    @Transactional
+    public void eliminarPedido(@Validated Pedido pedido){
+        pedidoRepository.delete(pedido);
+    }
     
 }
